53774cecc54eb3a4a5fcbceb4e6c236c1b7871dc
RAGHAV A <raghava.cse2024@citchennai.net>
2026-08-19 08:18:47 +0530

    Add Experiment 01 code

---
+++ Experiment-01-Installation-and-Exploration/experiment_01.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+import seaborn as sns
+import statsmodels.api as sm
+import scipy
+import plotly
+import bokeh
+import jupyterlab
+
+print("NumPy Version:", np.__version__)
+print("Pandas Version:", pd.__version__)
+print("Matplotlib Version:", matplotlib.__version__)
+print("Seaborn Version:", sns.__version__)
+print("Statsmodels Version:", sm.__version__)
+print("SciPy Version:", scipy.__version__)
+print("Plotly Version:", plotly.__version__)
+print("Bokeh Version:", bokeh.__version__)
+print("JupyterLab Version:", jupyterlab.__version__)
